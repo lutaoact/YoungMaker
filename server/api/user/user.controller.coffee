605db7aa3761f743ff097a2d881fc9ce4fc5ccc8
Chuan Wang <chuanwang@Chuans-MacBookPro.local>
2014-07-16 10:24:24 +0800

    add new coffee files

---
+++ server/api/user/user.controller.coffee
@@ -0,0 +1,89 @@
+'use strict'
+
+User = require './user.model'
+passport = require 'passport'
+config = require '../../config/environment'
+jwt = require 'jsonwebtoken'
+
+validationError = (res, err) ->
+  res.json 422, err
+
+###
+  Get list of users
+  restriction: 'admin'
+###
+exports.index = (req, res) ->
+  User.find {}, '-salt -hashedPassword', (err, users) ->
+    res.send 500, err if err
+    res.json 200, users
+
+###
+  Creates a new user
+###
+exports.create = (req, res, next) ->
+  newUser = new User req.body
+  newUser.provider = 'local'
+  newUser.save (err, user) ->
+    validationError res, err if err
+    token = jwt.sign
+      _id: user._id,
+      config.secrets.session,
+      expiresInMinutes: 60*5
+    res.json
+     token: token
+
+###
+  Get a single user
+###
+exports.show = (req, res, next) ->
+  userId = req.params.id
+
+  User.findById userId, (err, user) ->
+    next err if err
+    res.send 401 if not user
+    res.json user.profile
+
+###
+  Deletes a user
+  restriction: 'admin'
+###
+exports.destroy = (req, res) ->
+  User.findByIdAndRemove req.params.id, (err, user) ->
+    res.send 500, err if err
+    res.send 204
+
+###
+  Change a users password
+###
+exports.changePassword = (req, res, next) ->
+  userId = req.user._id
+  oldPass = String req.body.oldPassword
+  newPass = String req.body.newPassword
+
+  User.findById userId, (err, user) ->
+    if user.authenticate oldPass
+      user.password = newPass
+      user.save (err) ->
+        validationError res, err if err
+        res.send 200
+    else
+      res.send 403
+
+###
+  Get my info
+###
+exports.me = (req, res, next) ->
+  userId = req.user._id
+  User.findOne
+    _id: userId,
+    '-salt -hashedPassword',
+    (err, user) -> # donnot ever give out the password or salt
+      next err if err
+      res.json 401 if not user
+      res.json user
+
+###
+ Authentication callback
+###
+exports.authCallback = (req, res, next) ->
+  res.redirect '/'
---
+++ server/api/user/user.model.js
@@ -0,0 +1,176 @@
+(function() {
+  'use strict';
+  var ObjectId, Schema, UserSchema, createdModifiedPlugin, crypto, mongoose, validatePresenceOf;
+
+  mongoose = require('mongoose');
+
+  Schema = mongoose.Schema;
+
+  ObjectId = Schema.ObjectId;
+
+  createdModifiedPlugin = require('mongoose-createdmodified').createdModifiedPlugin;
+
+  crypto = require('crypto');
+
+  UserSchema = new Schema({
+    avatar: {
+      type: String
+    },
+    email: {
+      type: String,
+      lowercase: true
+    },
+    name: {
+      type: String
+    },
+    org_id: {
+      type: ObjectId
+    },
+    hashed_password: {
+      type: String
+    },
+    provider: {
+      type: String
+    },
+    role: {
+      type: String,
+      "default": 'student'
+    },
+    salt: {
+      type: String
+    },
+    status: {
+      type: String
+    }
+  });
+
+  UserSchema.plugin(createdModifiedPlugin);
+
+
+  /*
+  Virtuals
+   */
+
+  UserSchema.virtual('password').set(function(password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    return this.hashed_password = this.encryptPassword(password);
+  }).get(function() {
+    return this._password;
+  });
+
+  UserSchema.virtual('profile').get(function() {
+    return {
+      'name': this.name,
+      'role': this.role,
+      'avatar': this.avatar
+    };
+  });
+
+  UserSchema.virtual('token').get(function() {
+    return {
+      '_id': this._id,
+      'role': this.role
+    };
+  });
+
+
+  /*
+    Validations
+   */
+
+  UserSchema.path('email').validate(function(email) {
+    return email.length;
+  }, 'Email cannot be blank');
+
+  UserSchema.path('hashed_password').validate(function(hashed_password) {
+    return hashed_password.length;
+  }, 'Password cannot be blank');
+
+  UserSchema.path('email').validate(function(value, respond) {
+    var self;
+    self = this;
+    return this.constructor.findOne({
+      email: value
+    }, function(err, user) {
+      if (err) {
+        throw err;
+      }
+      if (user) {
+        if (self.id === user.id) {
+          respond(true);
+        }
+        respond(false);
+      }
+      return respond(true);
+    });
+  }, 'The specified email address is already in use.');
+
+  validatePresenceOf = function(value) {
+    return value && value.length;
+  };
+
+
+  /*
+    Pre-save hook
+   */
+
+  UserSchema.pre('save', function(next) {
+    if (!this.isNew) {
+      next();
+    }
+    if (!validatePresenceOf(this.hashed_password) && authTypes.indexOf(this.provider) === -1) {
+      return next(new Error('Invalid password'));
+    } else {
+      return next();
+    }
+  });
+
+
+  /*
+    Methods
+   */
+
+  UserSchema.methods = {
+
+    /*
+      Authenticate - check if the passwords are the same
+      @param {String} plainText
+      @return {Boolean}
+      @api public
+     */
+    authenticate: function(plainText) {
+      return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    /*
+     Make salt
+     @return {String}
+     @api public
+     */
+    makeSalt: function() {
+      return crypto.randomBytes(16).toString('base64');
+    },
+
+    /*
+      Encrypt password
+    
+      @param {String} password
+      @return {String}
+      @api public
+     */
+    encryptPassword: function(password) {
+      var salt;
+      if (!password || !this.salt) {
+        '';
+      }
+      salt = new Buffer(this.salt, 'base64');
+      return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+    }
+  };
+
+  module.exports = mongoose.model('User', UserSchema);
+
+}).call(this);
+
+//# sourceMappingURL=user.model.js.map

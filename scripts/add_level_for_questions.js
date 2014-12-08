864a33135f4e0a7077b59f391e59aeb3afb25d9a
zhenkunou <zhenkunou@cloud3edu.com>
2014-12-08 10:18:42 +0800

    refactor: remove old script

--- scripts/add_level_for_questions.js
+++
@@ -1,3 +0,0 @@
-db.questions.find().forEach(function(question) {
-  db.questions.update({_id: question._id}, {$set: {level: Math.ceil(Math.random() * 100)}});
-});
--- scripts/alter_mongodb.js
+++
@@ -1,1 +0,0 @@
-db.categories.update({}, {$set: {orgId: db.organizations.findOne({uniqueName: 'xqsh'})._id }}, {multi: true});//为所有的category添加orgId字段

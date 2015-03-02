df122da7d7e3185c664b7e159bb68705f973103c
taolu <taolu@cloud3edu.com>
2015-03-02 15:27:22 +0800

    create_crontab_update_heat.sh: change == to =

--- scripts/create_crontab_update_heat.sh
+++ scripts/create_crontab_update_heat.sh
@@ -1,6 +1,6 @@
 SCRIPTS_PATH=$(pwd)
 MONGO_PATH=$(which mongo)
-if [ "$NODE_ENV" == 'production' ]; then
+if [ "$NODE_ENV" = 'production' ]; then
   command="$MONGO_PATH 10.80.44.45/maui $SCRIPTS_PATH/crontab_update_heat.js 2>&1 >> /data/log/crontab.log"
 else
   command="$MONGO_PATH maui-dev $SCRIPTS_PATH/crontab_update_heat.js 2>&1 >> /data/log/crontab.log"
